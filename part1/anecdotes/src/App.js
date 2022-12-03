c4d3bfcd4bb1406dba630d4a9e1142bec30f277e
seth-mc <64566764+seth-mc@users.noreply.github.com>
2022-12-03 15:08:22 +0000

    anecdotes completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({})
+
+  const rand = getRandomInt(anecdotes.length);
+
+
+  const handleVotes = () => {
+    // sets a new const, newVotes, with all of the values in defined array "votes"
+    const newVotes = {...votes};
+    let current = 0;
+    // if the index has been defined, then add the current amount of votes to it
+    if (selected in newVotes) {
+      current += newVotes[selected]
+    }
+    // Add one to the tally of votes (because the user has already pressed the button). 
+    newVotes[selected] = current + 1
+    console.log(newVotes)
+    // set "votes" to edited object "newVotes". 
+    setVotes(newVotes);
+  }
+
+
+  const getHighestVote = () => {
+    if (Object.keys(votes).length === 0) {
+      return;
+    }
+    // return key of the highest value in the "votes" object
+    const max = Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b);
+    // use the key found and grab that quote from the "anecdotes array"
+    const quote = anecdotes[max];
+
+    return quote;
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br></br>
+      <br></br>
+      <Button onClick={() => setSelected(rand)} text="Next Anecdote"/>
+      <Button onClick={() => handleVotes()} text="vote"></Button>
+      <h1>Highest Voted Anecdote</h1>
+      {getHighestVote()}
+    </div>
+  )
+}
+
+export default App
